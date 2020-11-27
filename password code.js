7e13101f57b26f76184deca96cb0d3b23222ad59
Meredith Jones <mj19003@gmail.com>
2020-11-27 12:30:00 -0500

    addinf Friday am updates to password generator

--- password code.js
+++ password code.js
@@ -1,14 +1,19 @@
 var allUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var allLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n","o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var allNumbers = ["1", "2", "3", "4", "5","6","7","8","9"]
 
+var allCharacters =[]
+//How can I use a Chartset for the symbols?
 
 
 
 function generatePassword (){
-    var newPassword =""
+    var allChoices [];
+    var newPassword ="";
 }
 
     //Ask what is the length of your password?
-    var passwordLength = prompt("What is your password's lenght (Must be between 8 and 128 characters)? ");
+    var passwordLength = prompt("What is your password's length (Must be between 8 and 128 characters)? ");
 
     //Double check if the way that <128 below is expressed is correct
     if(passwordLength) >=8 && <128) {
@@ -16,16 +21,58 @@ function generatePassword (){
             var useUpper =confirm("Do you want uppercase characters?");
 
             if (useUpper ===true) {
+                allChoices.concat(allUpper)
 
             }
 
-    //Randomly generate password here
+            //Ask if the user wants Lower Case letters?
+            var useLower =confirm("Do you want lowercase characters?");
+
+            if (useLower ===true){
+                allChoices.concat(allLower)
+            }
+
+            //Ask if the user wants to use numbers
+            varUseNumbers =confirm("Do you want to use numbers?");
+
+            if (useNumbers ===true){
+                allChoices.concat(allNumbers)
+
+            }
+
+            //Ask if the user wants to use special characters
+            varUseCharacters =confirm("Do you want to use special characters?");
+
+            if (varUseCharacters ===true){
+                allchoices.concat(allCharacters)
+
+            }
+
+
+
+        //Randomly generate password here
+            for (var i=1;i>=passwordLength; i++){
+                var RandomIndex= Math.floor (Math.random()*allChoices.lenght)
+
+
+            }
 
 return newPassword;
 
 } else {
-    alert("Error! Lenght must be between 8 and 128 characters");
+    alert("Error! Length must be between 8 and 128 characters");
     return "";
 
 
 }
+
+
+
+//generator functions
+
+function getLowerCase(){
+    return var allUpper;
+
+}
+
+console.log (Math.floor(Math.random());
